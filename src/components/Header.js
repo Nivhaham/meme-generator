0e11ee3408cb87eadffc5f5c5bbfd930c7052b4c
Nivhaham <49638534+Nivhaham@users.noreply.github.com>
2023-11-21 21:30:35 +0200

    Committing

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            
+            <h1>Meme Generator</h1>
+            <h3>Niv's Meme Project</h3>
+        </div>
+    )
+}

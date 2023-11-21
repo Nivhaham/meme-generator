556aa28b7ec3812391b509f57d8be28312890f40
Nivhaham <49638534+Nivhaham@users.noreply.github.com>
2023-11-22 01:37:06 +0200

    Adding styling to site

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import frog from '../images/frog.png'
 export default function Header() {
     return (
-        <div>
-            
-            <h1>Meme Generator</h1>
-            <h3>Niv's Meme Project</h3>
+        <div className='header'>
+            <img src={frog} className='header--image' alt='frog meme' />
+            <h1 className='header--title'>Meme Generator </h1>
+            <h3 className='header--project'>Niv's Meme Project</h3>
         </div>
     )
 }
